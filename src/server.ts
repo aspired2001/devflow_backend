6699194f0c9281d6abb6d2485c090f5ce062ddaf
aspired2001 <100749624+aspired2001@users.noreply.github.com>
2026-04-15 17:13:50 +0530

    feat: integrate Prisma migration command in server startup

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import http from "http"
+import { execSync } from "child_process"
+execSync("npx prisma migrate deploy", { stdio: "inherit" })
 
+import http from "http"
 import dotenv from "dotenv"
 dotenv.config()
 
 import app from "./app"
 import { initSocket } from "./sockets"
 
-
-
 const server = http.createServer(app)
 
 initSocket(server)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
